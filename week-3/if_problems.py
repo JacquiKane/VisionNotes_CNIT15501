9150a86af8173c1f7584198201411f930ad4660d
ACEberly7 <aaroneberly911@gmail.com>
2025-03-30 22:02:01 -0400

    Add files via upload

---
+++ week-3/if_problems.py
@@ -0,0 +1,22 @@
+"""
+Aaron Eberly
+Jan 30 2025
+Illustrate if statements
+"""
+
+def main():
+    points = 72 # Try changing this variable to see how the output changes in response!
+    if (points >= 90):
+        print("A")
+    elif (points >= 80):
+        print("B")
+    elif (points >= 70):
+        print("C")
+    elif (points >= 60):
+        print("D")
+    else:
+        print("F")
+    
+    # Try replacing some of these elif statements with if statements. The code will not work as intended!
+
+main()
